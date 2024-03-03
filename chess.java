063639eb9ad1c2e59d3ea939a80f39b37a4f7f00
johnnnathan <johnnnathan@proton.me>
2024-03-03 13:15:50 +0100

    I think I finished the gameloop. Need to add castling and UI. And of course test it because I don't even know if it works

--- chess.java
+++ chess.java
@@ -1,12 +1,68 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 class chess {
+    public static char currentColor;
+    public static Piece currentPiece;
+    public static boolean gameActive = true;
+
     public static void main(String[] args) {
         System.out.println("TEST");
         Queen queen = new Queen(4,4,true);
         System.out.println(queen.getMoves());
     }
+
+    public static void GameLoop(){
+        Scanner scanner = new Scanner(System.in);
+        int decodedMove;
+        char currentColor = 'W';
+        while (gameActive){
+            System.out.println("Make your move");
+            decodedMove = Board.decodeMove(scanner.nextLine());
+            if (matchMove(decodedMove)){
+                Board.removePiece(decodedMove, invertColor(currentColor));
+                currentPiece.setX(decodedMove/1000);
+                currentPiece.setY(decodedMove%10);
+                currentColor = invertColor(currentColor);
+            }
+
+
+        }
+        currentColor = invertColor(currentColor);
+        if (currentColor == 'W') {
+            System.out.println("White won");
+        }else {
+            System.out.println("Black Won");
+        }
+
+
+    }
+    public static char invertColor(char Color){if (Color == 'W'){return 'B';}return 'W';}
+
+    public static boolean matchMove(int move){
+        int[] sae = Board.findStartAndEnd(currentColor);
+        for (int i = sae[0]; i < sae[1]; i++){
+            ArrayList<Integer> moves = Board.piecePositions[i].getMoves();
+            for (int tempMove: moves){
+                if (tempMove == move){
+                    currentPiece = Board.piecePositions[i];
+                    return (true);
+
+                }
+
+            }
+        }
+        return (false);
+
+
+    }
+    public void setCurrentColor(char color){currentColor = color;}
+    public char getCurrentColor(){return currentColor;}
+
+
+
+
 }
 abstract class Piece {
     private int locx, locy, modx, mody;
@@ -25,19 +81,13 @@ abstract class Piece {
         else {this.modx = -1; this.mody = 1;}
     }
 
-    public boolean acceptMove(ArrayList<Integer>moves, String move){
-        return true;
 
-
-
-
-
-
-    }
-
+    public char getRank(){return rank;}
     public int getID(){return pieceID;}
     public int getX(){return locx;}
     public int getY(){return locy;}
+    public void setX(int x){this.locx = x;}
+    public void setY(int y){this.locy = y;}
     public char getColor(){
         if (color){return 'W';}
         return 'B';
@@ -66,13 +116,13 @@ class Pawn extends Piece{
         int x = this.getX();
         int y = this.getY();
 
-        if (!Board.occupiedByParty(x, y + (1*this.getModY()), 'A'))    {moves.add(Board.quickFormat(x,y + (1*this.getModY())));}
+        if (!Board.occupiedByParty(x, y + (1*this.getModY()), 'A') && Board.isOnBoard(x,y+this.getModY())){moves.add(Board.quickFormat(x,y + (1*this.getModY())));}
         
-        if (!Board.occupiedByParty(x, y + (2*this.getModY()), 'A')&&firstMove){moves.add(Board.quickFormat(x,y + (2*this.getModY())));}
+        if (!Board.occupiedByParty(x, y + (2*this.getModY()), 'A')&& Board.isOnBoard(x,y+(2*this.getModY())) && firstMove){moves.add(Board.quickFormat(x,y + (2*this.getModY())));}
         
-        if (!Board.occupiedByParty(x + 1, y + (1*this.getModY()), getOpponent())) {moves.add(Board.quickFormat(x + 1,y + (this.getModY())));}
+        if (!Board.occupiedByParty(x + 1, y + (1*this.getModY()), getOpponent()) && Board.isOnBoard(x+1,y + this.getModY())) {moves.add(Board.quickFormat(x + 1,y + (this.getModY())));}
         
-        if (!Board.occupiedByParty(x - 1, y + (1*this.getModY()), getOpponent())) {moves.add(Board.quickFormat(x - 1,y + (this.getModY())));}
+        if (!Board.occupiedByParty(x - 1, y + (1*this.getModY()), getOpponent()) && Board.isOnBoard(x-1,y+this.getModY())) {moves.add(Board.quickFormat(x - 1,y + (this.getModY())));}
 
         return moves;
     }
@@ -109,7 +159,7 @@ class King extends Piece{
         ArrayList<Integer> moves = new ArrayList<>();
         for (int i = this.getX() - 1; i <= this.getX() + 1; i++){
             for (int j = this.getY() -1; j <= this.getY() + 1; i++){
-                if (this.getX()!=i && this.getY()!=j && !Board.occupiedByParty(i,j,this.getColor())){moves.add(Board.quickFormat(i,j));}
+                if (this.getX()!=i && this.getY()!=j && !Board.occupiedByParty(i,j,this.getColor()) && Board.isOnBoard(i,j)){moves.add(Board.quickFormat(i,j));}
             }
         }
         return moves;
@@ -275,14 +325,24 @@ class Rook extends Piece {
 }
 class Board{
     public static Piece[][] board = new Piece[8][8];//I don't think this will do anything. Keeping it for possible future use, should delete if not
-    public static int[] piecePositions = new int[32];//Keep all current positions of each piece depending on ID. Blacks should be in the back (x>15). And yes, that                                                     is a reference to segregation in america
+    public static Piece[] piecePositions = new Piece[32];//Keep all pieces depending on ID. Blacks should be in the back (x>15). And yes, th                                                                              //at is a reference to segregation in america
 
-    public static void occupyPosition(Piece piece){//assuming that position of piece has been updated in the object
-        piecePositions[piece.getID()] = piece.getXY();
-    }
 
     public static boolean occupiedByParty(int X, int Y, char party/*party should be A for (A)ll, "B" for (B)lack and "W" for (W)hite*/){
         int endPosition = quickFormat(X,Y);
+
+        int[]sae = findStartAndEnd(party);
+        boolean flag = false;
+        for (int i = sae[0]; i < sae[1]; i++){
+            int position = Board.quickFormat(piecePositions[i].getX(),piecePositions[i].getY());
+            if (position == endPosition){flag = true;break;}
+        }
+        return flag;
+
+
+    }
+
+    public static int[] findStartAndEnd(char party){
         int start =-1;
         int end = -1;//end should be one bigger than the position of the final element in order to immediately add it to the loop
 
@@ -300,14 +360,44 @@ class Board{
                 end = 16;
                 break;
         }
-        boolean flag = false;
-        for (int i = start; i < end; i++){
-            if (piecePositions[i] == endPosition){flag = true;break;}
-        }
-        return flag;
+        return(new int[]{start, end});
+
 
 
     }
+
+    public static int getPiecePositionFormated(Piece piece){
+        int x = piece.getX();
+        int y = piece.getY();
+        return Board.quickFormat(x,y);
+
+
+    }
+
+    public static int decodeMove(String move){
+        char charx = move.charAt(0);
+        int y = Character.getNumericValue(move.charAt(1));
+        int x = Character.getNumericValue(charx-17);
+        if (isOnBoard(x,y)){return quickFormat(x,y);}
+        else{return 999;}
+
+
+    }
+
+    public static void removePiece(int move, char party){
+        int[]sea = findStartAndEnd(party);
+        for (int i = sea[0]; i < sea[1]; i++){
+            int tempMove = getPiecePositionFormated(piecePositions[i]);
+            if (tempMove == move){
+                if (piecePositions[i].getRank() == 'K'){chess.gameActive = false;}
+                piecePositions[i] = null;
+            }
+
+        }
+
+
+    }
+
     public static int quickFormat(int X, int Y){return (X*1000)+Y;}//this is the same function as getXY from pieces by applying to any coordinates
     public static boolean isOnBoard(int x){if (x>=0 && x<=7){return true;}return false;}
     public static boolean isOnBoard(int x, int y){if (x>=0 && x<=7 && y>=0 && y<=7){return true;}return false;}
